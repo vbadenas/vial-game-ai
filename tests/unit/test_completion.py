bffa4c48c44372dbeadfdd3ce8486cc20f7ac128
Victor <victor.badenas@gmail.com>
2020-08-06 21:48:35 +0200

    fixed tests

--- tests/unit/test_completion.py
+++ tests/unit/test_completion.py
@@ -1,11 +1,18 @@
+import sys
 import unittest
-from src.main import BallSortGame
+sys.path.append("./src")
+from main import *
 
-class TestCompletion:
+class TestCompletion(unittest.TestCase):
     def test_completionOk(self):
         colors = 'bbbbkkkkggggyyyy'
         ballSortGame = BallSortGame(colors)
         self.assertTrue(ballSortGame.checkForCompletion())
+
+    def test_completionNOk(self):
+        colors = 'bgkyyykkgbbgbykg'
+        ballSortGame = BallSortGame(colors)
+        self.assertFalse(ballSortGame.checkForCompletion())
 
 if __name__ == "__main__":
     unittest.main()
