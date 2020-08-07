e8124d9fb1d8b26259ee0ccfc898b7d9d5131783
Victor <victor.badenas@gmail.com>
2020-08-07 21:00:09 +0200

    update position in ball

--- src/objects/ball.py
+++ src/objects/ball.py
@@ -3,11 +3,14 @@ import pygame
 class Ball:
     def __init__(self, color, position, radius):
         self.color = color
-        self.position = position
+        self.updatePosition(position)
         self.radius = radius
 
     def paint(self, screen, rgbcolors):
         pygame.draw.circle(screen, rgbcolors[self.color], self.position, self.radius)
+
+    def updatePosition(self, position):
+        self.position = position
 
     def __str__(self):
         return str(self.__dict__)
